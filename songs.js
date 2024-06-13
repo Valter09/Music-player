03e5db9083ea50160344d8fc90151934110663db
Valter <114238552+Valter09@users.noreply.github.com>
2024-06-12 21:28:40 -0300

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    src: "./src/Dude - Patrick Patrikios.mp3",
+    name: "Dude - Patrick Patrikios",
+  },
+  {
+    src: "./src/Girl Before A Mirror - Verified Picasso.mp3",
+    name: "Girl Before A Mirror - Verified Picasso",
+  },
+  {
+    src: "./src/Like That - Anno Domini Beats.mp3",
+    name: "Like That - Anno Domini Beats",
+  },
+];
